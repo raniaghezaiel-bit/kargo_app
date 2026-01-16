6a653f2ad6b00c6af8954bb44ebdcbbd23eb7a5c
Rania <rania.ghezaiel@sesame.com.tn>
2026-01-16 16:14:27 +0100

    Mise à jour du site web

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ReservationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,7 +12,7 @@ class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'app_dashboard')]
     #[IsGranted('ROLE_USER')]
-    public function index(): Response
+    public function index(ReservationRepository $reservationRepository): Response
     {
         $user = $this->getUser();
 
@@ -20,8 +21,16 @@ class DashboardController extends AbstractController
             return $this->render('dashboard/admin.html.twig');
         }
 
-        // Dashboard passager par défaut
-        return $this->render('dashboard/passager.html.twig');
+        // Dashboard passager avec ses dernières réservations
+        $reservations = $reservationRepository->findBy(
+            ['passager' => $user],
+            ['dateReservation' => 'DESC'],
+            5  // Limiter à 5 dernières réservations
+        );
+
+        return $this->render('dashboard/passager.html.twig', [
+            'reservations' => $reservations,
+        ]);
     }
 
     #[Route('/admin/dashboard', name: 'app_admin_dashboard')]
