abd1601aca7647ed90b025ab4dbf8425665d6860
Rania <rania.ghezaiel@sesame.com.tn>
2026-01-17 10:53:34 +0100

    Ajout gestion réservations admin, statistiques et mise à jour controllers

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -13,29 +13,33 @@ class SearchController extends AbstractController
     #[Route('/search/trajet', name: 'app_search_trajet', methods: ['GET'])]
     public function searchTrajet(Request $request, HoraireRepository $horaireRepository): Response
     {
-        $depart = $request->query->get('villeDepart');
-        $destination = $request->query->get('villeArrivee');
-        $date = $request->query->get('dateDepart');
+        // Récupérer les paramètres de recherche
+        $villeDepart = $request->query->get('villeDepart');
+        $villeArrivee = $request->query->get('villeArrivee');
+        $dateDepart = $request->query->get('dateDepart');
 
         $trajets = [];
 
-        if ($depart && $destination && $date) {
-            // Convertir la date string en DateTime
-            $dateObj = new \DateTime($date);
-            
-            // Rechercher les horaires disponibles
-            $trajets = $horaireRepository->findAvailableHoraires(
-                $depart,
-                $destination,
-                $dateObj
-            );
+        // Si tous les critères sont remplis, rechercher
+        if ($villeDepart && $villeArrivee) {
+            // Rechercher les horaires correspondants
+            $trajets = $horaireRepository->createQueryBuilder('h')
+                ->where('h.villeDepart = :depart')
+                ->andWhere('h.villeArrivee = :arrivee')
+                ->andWhere('h.statut = :statut')
+                ->setParameter('depart', $villeDepart)
+                ->setParameter('arrivee', $villeArrivee)
+                ->setParameter('statut', 'actif')
+                ->orderBy('h.heureDepart', 'ASC')
+                ->getQuery()
+                ->getResult();
         }
 
         return $this->render('search/results.html.twig', [
             'trajets' => $trajets,
-            'depart' => $depart,
-            'destination' => $destination,
-            'date' => $date,
+            'depart' => $villeDepart,
+            'destination' => $villeArrivee,
+            'date' => $dateDepart,
         ]);
     }
 }
